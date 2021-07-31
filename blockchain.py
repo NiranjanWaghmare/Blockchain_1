3858633ea61dad2c070f7ba106bb21eaeefb2827
Niranjan Waghmare <52606175+NiranjanWaghmare@users.noreply.github.com>
2021-07-31 18:40:28 +0530

    basic blockchain which creates blocks

---
+++ blockchain.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from index import Block
+
+block_chain = [Block.create_genesis_block()]
+# print("Hash: %s"%block_chain[-1].hash)
+# print("Hash: %s"%block_chain[-1].hash)
+num_of_blocks = 10
+
+for i in range(1, num_of_blocks+1):
+    block_chain.append(Block(block_chain[-1].hash,"DATA!!",datetime.now()))
+    print("Block #%d has been created: %s"%(i,block_chain[i].hash))
--- index.py
+++ index.py
@@ -1,1 +1,24 @@
-python
+import hashlib
+import datetime
+
+class Block:
+    def __init__(self,previous_hash,data,timestamp):
+        self.data = data
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.hash = self.get_hash()
+
+    def get_hash(self):
+        header_bin = (str(self.previous_hash)+str(self.data)+str(self.timestamp)).encode()
+        inner_hash = hashlib.sha256(header_bin).hexdigest().encode()
+        outer_hash = hashlib.sha256(inner_hash).hexdigest().encode()
+        return outer_hash
+
+    @staticmethod   
+    def create_genesis_block():
+        return Block("0","0",datetime.datetime.now())
+
+
+
+
+
